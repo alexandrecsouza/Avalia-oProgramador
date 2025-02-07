618082a769522e9eef43222aabd34d0e1e6c24af
Alexandre <xandycsouza@gmail.com>
2025-02-07 12:53:48 -0300

    criação tabelas

---
+++ database/migrations/2025_02_07_132400_create_vendas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            //$table->id();
+            //$table->timestamps();
+            /*
+            $table->string('id');
+            $table->string('id_cliente');
+            $table->string('id_loja');
+            $table->string('id_vendedor');
+            */
+            $table->string('id');
+            $table->string('data');
+            $table->string('valor');
+            $table->string('observacao');
+            $table->string('pagamento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendas');
+    }
+};
